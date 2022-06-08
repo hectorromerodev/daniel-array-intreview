f51c57755e60d25d81380c152bfcdaacdcdab614
Hector Romero <hectorromerodev@gmail.com>
2022-06-08 14:45:31 -0700

    ✨ Daniel test changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'daniel-array-intreview';
+  selected: string = 'Any selected item';
+
+  menu: Menu = {
+    id: 'menu-1',
+    name: 'My menu',
+    products: [
+      { id: '1', name: 'Product-1', price: 100 },
+      { id: '2', name: 'Product-2', price: 102 },
+      { id: '3', name: 'Product-3', price: 103 }
+    ]
+  }
+
+  select(item: Product) {
+    this.selected = item.name;
+  }
+
+}
+
+interface Menu {
+  id: string;
+  name: string;
+  products: Product[]
+}
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
 }
